fa780d910be15d9ba022bfb00e06809b912c6979
Abby <abbyyy23@gmail.com>
2017-03-04 02:53:36 -0600

    arranged competitions dataframe ensuring that the order is kept

--- Documents/Soccer/extract_links.py
+++ Documents/Soccer/extract_links.py
@@ -17,7 +17,6 @@ def main():
     competition_teams_df = json_normalize(sample)
 
     #save teams by competiton
-    #euro_list = competition_teams_df.teams[0]
     pl_list = competition_teams_df.teams[1]
     sa_list = competition_teams_df.teams[2]
     pd_list = competition_teams_df.teams[3]
@@ -28,11 +27,11 @@ def main():
 
     #create dataframe for teams of each competitoin
     #euro_teams = competition_teams(euro_list, 'euro_teams', 'Euro Cup')
-    pl_teams = competition_teams(pl_list, 'pl_teams', 'PL 16/17')
-    sa_teams = competition_teams(sa_list, 'sa_teams', 'SA 16/17')
-    pd_teams = competition_teams(pd_list, 'pd_teams', 'PD 16/17')
-    cl_teams = competition_teams(cl_list, 'cl_teams', 'CL 16/17')
-    bl_teams = competition_teams(bl_list, 'bl_teams', 'BL 16/17')
+    pl_teams = competition_teams(pl_list, 'pl_teams', 0)
+    sa_teams = competition_teams(sa_list, 'sa_teams', 1)
+    pd_teams = competition_teams(pd_list, 'pd_teams', 2)
+    cl_teams = competition_teams(cl_list, 'cl_teams', 3)
+    bl_teams = competition_teams(bl_list, 'bl_teams', 4)
     # to test out if dataframes can be exported to postgres as tables
     #engine = create_engine('postgresql://abbyparra@localhost:5432/dummyDB')
     #euro_teams.to_sql('euro_teams', engine)
@@ -43,14 +42,13 @@ def main():
     all_teams= pd.concat(frames)
     #reset the index of the dataframe
     all_teams.reset_index(drop=True, inplace=True)
-    #write all teams to csv file 
+    #write all teams to csv file
     #all_teams.to_csv("all_teams.csv", sep='\t', encoding='utf-8')
 
     #all_teams.to_pickle(root+ 'all_teams.pkl')
 
-    print all_teams
-
     #pp.pprint(teams)
+
 def competition_teams (comp_list, df_name, competition):
     df_name = pd.DataFrame()
     df_name['code'] = map(lambda team: team.get('code',None), comp_list)
--- Documents/Soccer/organize_data.py
+++ Documents/Soccer/organize_data.py
@@ -16,16 +16,55 @@ def main():
     #players_table = all_players(players_df)
     #players_table.to_pickle(root+ 'players_table.pkl')
 
-    all_teams = pd.read_pickle(root + 'all_teams.pkl')
+    teams_df = pd.read_pickle(root + 'all_teams.pkl')
+    players_df = pd.read_pickle(root + 'players_table.pkl')
+
 
     fixture_path = root + 'fixtures.json'
+    competition_path = root + 'competitions.json'
+    competitions_raw_df = fix_regular(competition_path)
     fixture_raw_df = data_frame_fixed(fixture_path)
     #copy the dataframe so it just contains the fixtures
     #fixture_df = fixture_raw_df['fixtures'].to_frame()
+    #print competitions_raw_df
+    competitions_df = competitions(competitions_raw_df)
+    #print competitions_df
+    fixture_df = all_fixtures(fixture_raw_df)
     
 
-    fixture_df = all_fixtures(fixture_raw_df)
-    print type(fixture_df['competitonID'])
+
+#keep just the competitons of interest
+def competitions(data):
+    #list with the name of columns to be dropped
+    columns_r = ['_links.fixtures.href','_links.leagueTable.href',
+    '_links.self.href','_links.teams.href','currentMatchday','id',
+    'lastUpdated']
+    #remove the columns listed above from the dataframe
+    data.drop(columns_r, axis=1, inplace=True)
+
+    # in order to have the competitions in the desired order had to do all this
+    # since the reindex function was not doing it
+
+    #converting data back to  a dictionary after unecessary columns removed
+    dic_data = data.to_dict(orient='records')
+    #insert the desired competitions in the order that was needed
+    new_df = pd.DataFrame()
+    new_df = new_df.append(dic_data[1], ignore_index = True)
+    new_df = new_df.append(dic_data[12], ignore_index = True)
+    new_df = new_df.append(dic_data[10], ignore_index = True)
+    new_df = new_df.append(dic_data[14], ignore_index = True)
+    new_df = new_df.append(dic_data[4], ignore_index = True)
+
+    return new_df
+
+
+
+
+###############################################################################
+#the following functions are used to arrange the fixtures and results dataframe#
+#fixture_get_columns, results_get_columns, goalsAway, goalsHome, homeWin,      #
+#awayWin, draw, all_fixtures                                                   #
+###############################################################################
 
 def all_fixtures(fixtures_df):
     index_values = fixtures_df
@@ -42,7 +81,6 @@ def all_fixtures(fixtures_df):
     all_competitions = pd.concat(df_name)
     return all_competitions
 
-
 def fixture_get_columns(data):
     df_name = pd.DataFrame()
     df_name['status'] = map(lambda fixture: fixture.get('status', None), data)
@@ -50,7 +88,58 @@ def fixture_get_columns(data):
     df_name['matchday'] = map(lambda fixture: fixture.get('matchday', None), data)
     df_name['homeTeamName'] = map(lambda fixture: fixture.get('homeTeamName', None), data)
     df_name['awayTeamName'] = map(lambda fixture: fixture.get('awayTeamName', None), data)
+    #df_name['odds'] = map(lambda fixture: fixture.get ('odds', None), data)
     return df_name
+
+def results_get_columns(data):
+    df_name = pd.DataFrame()
+    df_name['goalsHomeTeam'] = map(goalsHome, data)
+    df_name['goalsAwayTeam'] = map(goalsAway, data)
+    df_name['oddsHomeWin'] = map(homeWin, data)
+    df_name['oddsAwayWin'] = map(homeWin, data)
+    df_name['draw'] = map(draw, data)
+
+    return df_name
+
+def goalsAway(data):
+    result = data.get('result', None)
+    if result == None:
+        return None
+    goals_away = result.get('goalsAwayTeam', None)
+    return goals_away
+
+def goalsHome(data):
+    result = data.get('result', None)
+    if result == None:
+        return None
+    goals_home = result.get('goalsHomeTeam', None)
+    return goals_home
+
+def homeWin(data):
+    odds = data.get('odds', None)
+    if odds == None:
+        return None
+    home_win = odds.get('homeWin', None)
+    return home_win
+
+def awayWin(data):
+    odds = data.get('odds', None)
+    if odds == None:
+        return None
+    away_win = odds.get('awayWin', None)
+    return away_win
+
+def draw(data):
+    odds = data.get('odds', None)
+    if odds == None:
+        return None
+    away_win = odds.get('draw', None)
+    return away_win
+
+###############################################################################
+#the following functions are used to arrange the players dataframe            #
+#all_players and player_get_columns                                           #
+###############################################################################
 
 def all_players(players_df):
     index_values = players_df.index.values
@@ -67,8 +156,6 @@ def all_players(players_df):
     all_teams= pd.concat(df_name)
     return all_teams
 
-
-
 #to organize the players data into columns
 def player_get_columns(data):
     df_name = pd.DataFrame()
@@ -81,6 +168,12 @@ def player_get_columns(data):
     df_name['nationality'] = map(lambda player: player.get('nationality',None), data)
     df_name['position'] = map(lambda player: player.get('position',None), data)
     return df_name
+#normalizes regular json data
+def fix_regular(file_path):
+    data = open_file(file_path)
+    df_name = json_normalize(data)
+    return df_name
+
 
 # fixes the unformatted json data into dataframe
 def data_frame_fixed(data_path):
@@ -107,6 +200,12 @@ def loads_invalid_obj_list(s):
         objs.append(obj)
 
     return objs
+#opens file
+def open_file(file_path):
+    with open (file_path) as json_file:
+        input_file = json.load(json_file)
+    return input_file
+
 
 if __name__ == '__main__':
     main()
